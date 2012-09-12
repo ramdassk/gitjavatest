850998b56917d3c996ea9d22dcd2c14ae97e334b
ramdass <ramdassk@hotmail.com>
2012-09-12 20:27:09 +0530

    Committing from IntelliJ

--- src/GitJavaTest.java
+++ src/GitJavaTest.java
@@ -12,6 +12,6 @@ public class GitJavaTest {
     }
     public void printMessage()
     {
-        System.out.println (" Commit on Branch:.");
+        System.out.println (" Committing from IntelliJ");
     }
 }
